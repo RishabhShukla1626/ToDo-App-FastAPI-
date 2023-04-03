6ea219b6d6c469d68ce3490e258f13dd6f909527
RishabhShukla1626 <shukla.rishabh487@gmail.com>
2023-04-03 14:26:30 +0530

    Added user login/logout and register functionality(User Authentication)

--- ToDo/main.py
+++ ToDo/main.py
@@ -5,6 +5,8 @@ from db import engine
 from routers import auth, todos
 from companies import companyapis, dependencies
 from starlette.staticfiles import StaticFiles
+from starlette import status
+from starlette.responses import RedirectResponse
 
 
 
@@ -14,6 +16,11 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url='/todos', status_code=status.HTTP_302_FOUND)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
